1430aae7a10fb719ba91ace04d5b68eca7b3700f
Inha Hwang <inhah@uci.edu>
2026-02-09 17:22:53 -0800

    Added missing method

--- backend/src/main/java/com/maroom/maroom/repository/SavedListRepository.java
+++ backend/src/main/java/com/maroom/maroom/repository/SavedListRepository.java
@@ -4,8 +4,10 @@ import com.maroom.maroom.domain.SavedList;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface SavedListRepository extends JpaRepository<SavedList, UUID> {
     List<SavedList> findByUserId(UUID userId);
+    Optional<SavedList> findFirstByUserIdAndName(UUID userId, String name);
 }
