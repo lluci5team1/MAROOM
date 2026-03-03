43ea98866b783613332c20d582595acd3b0de76e
Daniel Kim <danihk15@uci.edu>
2026-03-02 16:40:49 -0800

    Furniture getAll

--- backend/src/main/java/com/maroom/maroom/controller/FurnitureItemController.java
+++ backend/src/main/java/com/maroom/maroom/controller/FurnitureItemController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 import java.util.UUID;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/furniture-items")
@@ -21,6 +22,12 @@ public class FurnitureItemController {
     @PostMapping
     public FurnitureItem create(@RequestBody FurnitureItem item) {
         return repo.save(item);
+    }
+
+
+    @GetMapping
+    public ResponseEntity<List<FurnitureItem>> getAll() {
+    return ResponseEntity.ok(repo.findAll());
     }
 
     @GetMapping("/{id}")
